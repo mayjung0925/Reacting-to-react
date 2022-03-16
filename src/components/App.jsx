6831eff7b05fc1574cf773e6b29fd6c32deb267d
Eunjin Jeoung <mayjung0925@gmail.com>
2022-03-16 11:38:28 -0500

    changed if~else statement to make it more readable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
-
-import ReactDOM from "react-dom";
 import Greeter from "./Greeter";
 
 const App = () => {
     const [username, setUsername] = useState('');
     const [loaded, setLoaded] = useState(false);
 
-    const newUser = (e) => {
+    const handleUsername = (e) => {
         setUsername(e.target.value)
     }
 
@@ -20,13 +18,26 @@ const App = () => {
             setLoaded(true)
 
         }, 3000);
-    }, [loaded])
+    }, [])// it runs one time only when the page is loading 
 
 
 
-    if (loaded == false) {
+    if (loaded) {
         return (
-            <div className="card text-center w-50 h-25 ">
+        <div className="text-center justify-content-center">
+            <h1 className="text-center" >Hello World!</h1>
+            <Greeter phrase="Hey" name="Luke" />
+            <Greeter phrase="Howdy" name="Jenna" />
+            <Greeter phrase="What's up" name="Lannie" />
+            <input value={username} onChange={handleUsername} />
+            <p>"You are logging in as :{username}</p>
+
+        </div>
+           
+        );
+    }
+    return (
+           <div className="card text-center w-50 h-25 ">
                 <h5 className="card-header">Reacting to React</h5>
                 <div className="card-body">
                     <h5 className="card-title">Website is loading</h5>
@@ -34,26 +45,9 @@ const App = () => {
                     <button type="button" className="btn btn-outline-primary" onClick={handleToggle}>click</button>
                 </div>
             </div>
-        );
-    }
-    return (
 
-        <div className="text-center justify-content-center">
-            <h1 className="text-center" >Hello World!</h1>
-            <Greeter phrase="Hey" name="Luke" />
-            <Greeter phrase="Howdy" name="Jenna" />
-            <Greeter phrase="What's up" name="Lannie" />
-            <input value={username} onChange={newUser} />
-            <p>"You are logging in as :{username}</p>
-
-        </div>
 
     );
-
-
-
-
-
 
 
 }
